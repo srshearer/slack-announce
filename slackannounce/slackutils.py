6fb97029d151cb39c72a81a326a89acac639f960
Steven Shearer <sshearer@apple.com>
2020-12-20 11:45:05 -0800

    * Remove python 2 support * Support python 3 properly * Add requirements.txt * Remove setup.py

--- slackannounce/slackutils.py
+++ slackannounce/slackutils.py
@@ -1,6 +1,4 @@
-#!/usr/bin/python -u
-# encoding: utf-8
-from __future__ import print_function, unicode_literals, absolute_import
+#!/usr/bin/env python3
 import json
 import requests
 
@@ -117,7 +115,7 @@ def text_color(requested_color):
         'blue': _colors.blue,
     }
 
-    if requested_color.lower() in map(unicode.lower, text_color_dict):
+    if requested_color.lower() in map(str.lower, text_color_dict):
         return_color = text_color_dict[requested_color]
     else:
         print('ERROR - Invalid color: {}'.format(requested_color))
