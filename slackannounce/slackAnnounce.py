5f3cffdcbf36e2a44b8ff6610efd93fb3013dd68
Steven Shearer <sshearer@apple.com>
2020-12-20 12:30:07 -0800

    * Updated code-style: replace single with double-quotes, replace `.format()` with f-strings, ran black against files * Replaced text color enum, dictionary, and function with python 3 dataclass * Deprecated `text_color()` function, but left it functional for now

--- slackannounce/slackAnnounce.py
+++ slackannounce/slackAnnounce.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 import sys
 import os.path
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 import argparse
 from slackannounce import config
 from slackannounce import slackutils
@@ -9,59 +10,101 @@ from slackannounce import slackutils
 
 def parse_arguments():
     parser = argparse.ArgumentParser(
-        description='Send messages to slack channel. Capable of sending custom '
-                    'messages, maintenance up/down messages.')
-    parser.add_argument('-c', '--color', dest='color', metavar='<color>',
-                        default=None, required=False, action='store',
-                        help='The color for message. Options: info (default), '
-                             'green/good, orange/warn, red/danger, purple')
-    parser.add_argument('-d', '--debug', dest='debug', default=False,
-                        required=False, action='store_true',
-                        help='Enable debug mode. Send message to test channel.')
-    parser.add_argument('--dry', dest='dryrun', default=False,
-                        required=False, action='store_true',
-                        help='Enable dryrun mode. Message will not be sent.')
-    parser.add_argument('-m', '--message', dest='message', metavar='<message>',
-                        default=None, required=True, action='store',
-                        help='The message to send to the channel.')
-    parser.add_argument('-r', '--channel', dest='channel', metavar='<channel>',
-                        default=None, required=False, action='store',
-                        help='Slack channel room to send the message to.')
-    parser.add_argument('-t', '--title', dest='title', metavar='<title>',
-                        default=None, required=False, action='store',
-                        help='Set a message title.')
+        description="Send messages to slack channel. Capable of sending custom "
+        "messages, maintenance up/down messages."
+    )
+    parser.add_argument(
+        "-c",
+        "--color",
+        dest="color",
+        metavar="<color>",
+        default=None,
+        required=False,
+        action="store",
+        help="The color for message. Options: info (default), "
+        "green/good, orange/warn, red/danger, purple",
+    )
+    parser.add_argument(
+        "-d",
+        "--debug",
+        dest="debug",
+        default=False,
+        required=False,
+        action="store_true",
+        help="Enable debug mode. Send message to test channel.",
+    )
+    parser.add_argument(
+        "--dry",
+        dest="dryrun",
+        default=False,
+        required=False,
+        action="store_true",
+        help="Enable dryrun mode. Message will not be sent.",
+    )
+    parser.add_argument(
+        "-m",
+        "--message",
+        dest="message",
+        metavar="<message>",
+        default=None,
+        required=True,
+        action="store",
+        help="The message to send to the channel.",
+    )
+    parser.add_argument(
+        "-r",
+        "--channel",
+        dest="channel",
+        metavar="<channel>",
+        default=None,
+        required=False,
+        action="store",
+        help="Slack channel room to send the message to.",
+    )
+    parser.add_argument(
+        "-t",
+        "--title",
+        dest="title",
+        metavar="<title>",
+        default=None,
+        required=False,
+        action="store",
+        help="Set a message title.",
+    )
     args = parser.parse_args()
     return args
 
 
 def set_message(args, **kwargs):
-    _message = args.message or kwargs.get('message') or None
-    _title = args.title or kwargs.get('title') or None
-    _color = args.color or kwargs.get('color') or None
+    _message = args.message or kwargs.get("message") or None
+    _title = args.title or kwargs.get("title") or None
+    _color = args.color or kwargs.get("color") or None
 
     if not args.message:
-        raise slackutils.SlackException('No message given: {}'.format(args))
+        raise slackutils.SlackException(f"No message given: {args}")
 
-    if args.message.startswith('up'):
-        title = 'Announcement: Server is up'
-        message = 'The server is back up!'
-        color = slackutils.text_color('good')
-    elif args.message.startswith('down '):
-        downtime = ' '.join(args.message.split(' ')[1:])
-        title = 'Announcement: Server going down'
-        message = 'The server is going down for maintenance.\n' \
-                  'Exepected downtime is about {}.'.format(downtime)
-        color = slackutils.text_color('warn')
-    elif _message.startswith('serverupdate'):
+    if args.message.startswith("up"):
+        title = "Announcement: Server is up"
+        message = "The server is back up!"
+        color = slackutils.TextColors.good
+    elif args.message.startswith("down "):
+        downtime = " ".join(args.message.split(" ")[1:])
+        title = "Announcement: Server going down"
+        message = (
+            "The server is going down for maintenance.\n"
+            f"Exepected downtime is about {downtime}."
+        )
+        color = slackutils.TextColors.warn
+    elif _message.startswith("serverupdate"):
         _m_list = _message.split(" ")
         software = _m_list[1]
-        title = '{} Update Available'.format(software)
-        message = ' '.join(_m_list[2:]).replace("\\n", "\n")
-        color = slackutils.text_color('blue')
+        title = f"{software} Update Available"
+        message = " ".join(_m_list[2:]).replace("\\n", "\n")
+        color = slackutils.TextColors.blue
     else:
         title = args.title or config.DEFAULT_TITLE
         message = args.message
-        color = args.color or slackutils.text_color('info')
+        color = args.color or slackutils.TextColors.info
 
     json_attachments = {
         "fallback": title,
@@ -77,7 +120,7 @@ def set_channel_and_webhook_url(args):
     if args.dryrun:
         args.debug = True
 
-    if args.channel == 'me':
+    if args.channel == "me":
         webhook_url = config.SLACK_WEBHOOK_URL_ME
         channel = None
     elif args.debug:
@@ -88,8 +131,8 @@ def set_channel_and_webhook_url(args):
         channel = config.DEFAULT_SLACK_ROOM
 
     if channel:
-        if list(channel)[0] != '#':
-            channel = '#' + channel
+        if list(channel)[0] != "#":
+            channel = "#" + channel
 
     return channel, webhook_url
 
@@ -103,7 +146,7 @@ def send_slack_message(args):
         user=config.DEFAULT_SLACK_USER,
         json_attachments=json,
         debug=args.debug,
-        dryrun=args.dryrun
+        dryrun=args.dryrun,
     )
     slack.send()
 
@@ -113,5 +156,5 @@ def main():
     send_slack_message(args)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- slackannounce/slackutils.py
+++ slackannounce/slackutils.py
@@ -1,27 +1,35 @@
 #!/usr/bin/env python3
 import json
 import requests
+from dataclasses import dataclass
 
 
 class SlackException(Exception):
     """Custom exception for Slack related failures."""
+
     pass
 
 
 class SlackSender(object):
-    def __init__(self, webhook_url=None, channel=None, user=None,
-                 json_attachments=None, debug=False, dryrun=False):
+    def __init__(
+        self,
+        webhook_url=None,
+        channel=None,
+        user=None,
+        json_attachments=None,
+        debug=False,
+        dryrun=False,
+    ):
         self.debug = debug
         self.dryrun = dryrun
         self.webhook_url = webhook_url
         self.json_attachments = json_attachments
         self.user = user
         self.channel = channel
-        self.color = text_color('info')
+        self.color = TextColors.info
         self._json_payload = None
 
-    def set_simple_message(self, message, title=None,
-                           color=None, fallback=None):
+    def set_simple_message(self, message, title=None, color=None, fallback=None):
         """Sets formatted json to use in sending a Slack notification using only
         a provided message.
         Requires:
@@ -56,71 +64,82 @@ class SlackSender(object):
         This will update the debug state, channel, and webhook before sending.
         """
         if not self.user:
-            raise SlackException('Missing user')
+            raise SlackException("Missing user")
         if not self.webhook_url:
-            raise SlackException('Missing webhook url')
+            raise SlackException("Missing webhook url")
         if not self.json_attachments:
-            raise SlackException('json_attachments not set')
+            raise SlackException("json_attachments not set")
 
         self._json_payload = {
             "channel": self.channel,
             "username": self.user,
-            "attachments": [
-                self.json_attachments
-            ]
+            "attachments": [self.json_attachments],
         }
 
         if self.debug:
-            print('json payload: {}'.format(self._json_payload))
+            print(f"json payload: {self._json_payload}")
 
         if self.dryrun:
-            print('[Dry run. Not posting message.]')
+            print("[Dry run. Not posting message.]")
             return
 
         response = requests.post(
-            self.webhook_url, data=json.dumps(self._json_payload),
-            headers={'Content-Type': 'application/json'}
+            self.webhook_url,
+            data=json.dumps(self._json_payload),
+            headers={"Content-Type": "application/json"},
         )
-        print('Result: [{}] {}'.format(response.status_code, response.text))
+        print(f"Result: [{response.status_code}] {response.text}")
 
         return response
 
 
-def enum(**enums):
-    """Defined enums type for convenience."""
-    return type(str('Enum'), (), enums)
+@dataclass
+class TextColors:
+    grey = "#d3d3d3"
+    green = "good"
+    orange = "warning"
+    red = "danger"
+    purple = "#764FA5"
+    blue = "#439FE0"
+
+    default = grey
+    info = grey
+    good = green
+    warn = orange
+    warning = orange
+    orange = orange
+    danger = red
+    red = red
+    purple = purple
 
 
 def text_color(requested_color):
     """Takes a color alias (str) and returns the color value if available"""
-    _colors = enum(
-        grey='#d3d3d3',
-        green='good',
-        orange='warning',
-        red='danger',
-        purple='#764FA5',
-        blue='#439FE0',
+
+    print(
+        "WARNING: This funtion is deprecated and will be removed soon! "
+        "Please use TextColors dataclass instead of text_color() function!"
     )
 
     text_color_dict = {
-        'default': _colors.grey,
-        'info': _colors.grey,
-        'good': _colors.green,
-        'green': _colors.green,
-        'warn': _colors.orange,
-        'orange': _colors.orange,
-        'danger': _colors.red,
-        'red': _colors.red,
-        'purple': _colors.purple,
-        'blue': _colors.blue,
+        "default": TextColors.grey,
+        "info": TextColors.grey,
+        "good": TextColors.green,
+        "green": TextColors.green,
+        "warn": TextColors.orange,
+        "orange": TextColors.orange,
+        "danger": TextColors.red,
+        "red": TextColors.red,
+        "purple": TextColors.purple,
+        "blue": TextColors.blue,
     }
 
     if requested_color.lower() in map(str.lower, text_color_dict):
         return_color = text_color_dict[requested_color]
     else:
-        print('ERROR - Invalid color: {}'.format(requested_color))
-        print('Available colors: {}'.format(list(text_color_dict)))
-        return_color = text_color_dict['default']
-        print('Returning default color: '.format(return_color))
+        print(f"ERROR - Invalid color: {requested_color}")
+        print(f"Available colors: {list(text_color_dict)}")
+        return_color = text_color_dict["default"]
+        print(f"Returning default color: {return_color}")
 
     return return_color
